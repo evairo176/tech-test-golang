f278c71e5595681fe2f9d934324d29f6eaf05e20
Dicki Prasetya <dickiprasetya176@gmail.com>
2026-06-29 16:38:22 +0000

    Add SQL scripts: create table, insert data, select/stored procedure

---
+++ sql/01_create_table.sql
@@ -0,0 +1,5 @@
+-- 1. Create Table Script
+CREATE TABLE IF NOT EXISTS Person (
+    Name    TEXT PRIMARY KEY,
+    Country TEXT NOT NULL
+);
---
+++ sql/02_insert_data.sql
@@ -0,0 +1,6 @@
+-- 2. Insert Data Script
+INSERT INTO Person (Name, Country) VALUES
+    ('Adam', 'Kuala Lumpur'),
+    ('John', 'Singapore'),
+    ('Henry', 'Singapore'),
+    ('Dominic', 'Thailand');
